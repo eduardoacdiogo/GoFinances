f16286ae37aba88e4ea78357fc6157db3a374faa
Eduardo Andre <educdiogo@hotmail.com>
2021-01-20 15:56:33 -0300

    backend

---
+++ backend/src/services/DeleteTransactionService.ts
@@ -0,0 +1,22 @@
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+class DeleteTransactionService {
+  public async execute(id: string): Promise<void> {
+    // TODO
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactionRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('The transaction does not exist');
+    }
+
+    await transactionRepository.remove(transaction);
+  }
+}
+
+export default DeleteTransactionService;
